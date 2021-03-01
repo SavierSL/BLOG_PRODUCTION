42cda7bb4ab7b906a82701ab2529e8ec9280979d
Xavier San Lorenzo <68961639+SavierSL@users.noreply.github.com>
2021-03-02 00:42:58 +0800

    Update settings.ts

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,5 +1,5 @@
 export const settings = {
   mongoURI:
-    "mongodb://SavierSL:xxkaa548@cluster0-shard-00-00.qkykc.mongodb.net:27017,cluster0-shard-00-01.qkykc.mongodb.net:27017,cluster0-shard-00-02.qkykc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-l8lb2k-shard-0&authSource=admin&retryWrites=true&w=majority",
+    "mongodb://SavierSL:secret123@cluster0-shard-00-00.qkykc.mongodb.net:27017,cluster0-shard-00-01.qkykc.mongodb.net:27017,cluster0-shard-00-02.qkykc.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-l8lb2k-shard-0&authSource=admin&retryWrites=true&w=majority",
   jwtSecret: "xavier",
 };
